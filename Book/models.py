03cbe6cdd546f3aae988241f0b0a2fe68c6a1ba8
Rohit_Git <gaikwadrohit1428@gmail.com>
2023-12-11 22:46:08 +0530

    update file

--- Book/models.py
+++ Book/models.py
@@ -42,3 +42,17 @@ class FileUpload(models.Model):
         db_table = "file_upload"
 
 
+
+
+
+class name(models.Model):
+    name = models.CharField(max_length=12)
+    company = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = "Name"
+
+
